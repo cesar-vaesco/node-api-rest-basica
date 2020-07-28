4b844b9e3afcd8162ed3b745c41a97b4057c7ed1
Cesar <cesarvargasescorcia@gmail.com>
2020-07-27 23:38:08 -0500

    api-basica: configuración inicial de servidor  -  creacion de carpeta routes

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+//Settings - configuraciones
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+// Middlewares
+app.use(morgan('short')); //Formato de la respuesta
+app.use(express.urlencoded({ extended: false })); //Datos que soporta la respuesta
+app.use(express.json()); //Formato de la respuesta
+
+//routes
+app.use(require('./routes/index'));
+// Inicializando el servidor
+app.listen(app.get('port'), () => {
+    console.log(`Servidor creado en el puerto ${app.get('port')}`);
+});
