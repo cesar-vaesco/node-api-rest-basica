4e49043064453d327580ec3b46fc90793ead2dc3
Cesar <cesarvargasescorcia@gmail.com>
2020-07-28 12:50:42 -0500

    api-basica: modificando peliculas - método put

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -20,6 +20,26 @@ router.post('/', (req, res, next) => {
     }
 });
 
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, director, year, rating } = req.body;
+    // Comprobar que los datos que se quieren actualizar existen
+    if (title && director && year && rating) {
+        // Recorrer el arreglo de peliculas
+        _.each(movies, (movie, i) => {
+            if (movie.id == id) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.json(movies);
+    } else {
+        res.status(500).json({ error: "Invalid" });
+    }
+});
+
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
     _.each(movies, (movie, i) => {
